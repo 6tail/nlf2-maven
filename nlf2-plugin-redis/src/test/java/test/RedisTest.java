20b7534f2c41ee65385753e41f89cc1e394a4d1d
6tail <6tail@6tail.cn>
2019-02-12 15:07:29 +0800

    add:more test

--- nlf2-plugin-redis/src/test/java/test/RedisTest.java
+++ nlf2-plugin-redis/src/test/java/test/RedisTest.java
@@ -4,12 +4,29 @@ import com.nlf.extend.dao.noSql.NoSqlDaoFactory;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Set;
+
 public class RedisTest {
 
   @Test
-  public void test() {
-    NoSqlDaoFactory.getDao().set("hello","world");
-    String hello = NoSqlDaoFactory.getDao().get("hello");
-    Assert.assertEquals("world",hello);
+  public void testSetGet() {
+    NoSqlDaoFactory.getDao().set("test:foo","bar");
+    String bar = NoSqlDaoFactory.getDao().get("test:foo");
+    Assert.assertEquals("bar",bar);
+  }
+
+  @Test
+  public void testDelete() {
+    NoSqlDaoFactory.getDao().delete("test:foo");
+    String bar = NoSqlDaoFactory.getDao().get("test:foo");
+    Assert.assertNull(bar);
+  }
+
+  @Test
+  public void testKeys() {
+    NoSqlDaoFactory.getDao().set("test:key:1","1");
+    NoSqlDaoFactory.getDao().set("test:key:2","2");
+    Set<String> keys = NoSqlDaoFactory.getDao().keys("test:key:*");
+    Assert.assertEquals(2,keys.size());
   }
 }
